a77b08159710c8190fc9d5d79d19fe58b23ccad9
jamesnmclaughlin <jamesnicholasmclaughlin@gmail.com>
2019-10-09 16:23:42 +0100

    Map Idea

---
+++ map.py
@@ -0,0 +1,25 @@
+from items import *
+
+location_home = {
+    "name": "Home",
+
+    "description": "",
+
+    "exits": {"east": "Tree", "north": "Castle", "west": "Bakery"}
+}
+
+location_Bakery = {
+    "name": "Bakery",
+
+    "description": "",
+
+    "exits" = {"south": "Town", "east": "Home", "north": "Woods"}
+}
+
+location_wood = {
+    "name": "Woods",
+
+    "description": "",
+
+    "exits" = {"south": "Farm"}
+}
